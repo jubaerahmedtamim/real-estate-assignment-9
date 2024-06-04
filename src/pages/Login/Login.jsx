5acdf773bb0a054d549cdafa5a77c753db0db9ee
jubaerahmedtamim <jubaerahmedtamim2014@gmail.com>
2024-06-04 12:43:00 +0600

    login & register page design done as well as firebase with .env.local for git ignore

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div>
-            This is login
+        <div className='my-6 max-w-lg mx-auto'> 
+            <h3 className='text-4xl font-semibold text-center'>Please Login</h3>
+            <form className="card-body">
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Email</span>
+                    </label>
+                    <input type="email" placeholder="email" className="input input-bordered" required />
+                </div>
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Password</span>
+                    </label>
+                    <input type="password" placeholder="password" className="input input-bordered" required />
+                    <label className="label">
+                        <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                    </label>
+                </div>
+                <div className="form-control mt-6">
+                    <button className="btn btn-primary">Login</button>
+                </div>
+            </form>
+            <p className='text-center'>Don't have an account? <Link to='/register' className='text-blue-600 font-bold'>Register</Link></p>
         </div>
     );
 };
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,9 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
-        <div>
-            this is register
+        <div className='my-6 max-w-lg mx-auto'> 
+            <h3 className='text-4xl font-semibold text-center'>Register</h3>
+            <form className="card-body">
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Name</span>
+                    </label>
+                    <input type="text" placeholder="name" className="input input-bordered" required />
+                </div>
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Email</span>
+                    </label>
+                    <input type="email" placeholder="email" className="input input-bordered" required />
+                </div>
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Photo URL</span>
+                    </label>
+                    <input type="text" placeholder="photo url" className="input input-bordered"  />
+                </div>
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Password</span>
+                    </label>
+                    <input type="password" placeholder="password" className="input input-bordered" required />
+                    <label className="label">
+                        <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                    </label>
+                </div>
+                <div className="form-control mt-6">
+                    <button className="btn btn-primary">Register</button>
+                </div>
+            </form>
+            <p className='text-center'>Already have an account? <Link to='/login' className='text-blue-600 font-bold'>Login</Link></p>
         </div>
     );
 };
