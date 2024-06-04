1bda925815499a877528f782bd94a3950a9cee9f
jubaerahmedtamim <jubaerahmedtamim2014@gmail.com>
2024-06-04 13:15:15 +0600

    not found page done

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -3,12 +3,14 @@ import Root from "../layout/Root";
 import Home from "../pages/Home/Home";
 import Login from "../pages/Login/Login";
 import Register from "../pages/Register/Register";
+import NotFound from "../pages/404NotFound/NotFound";
 
 
 export const router = createBrowserRouter([
     {
         path: "/",
         element:<Root></Root>,
+        errorElement:<NotFound></NotFound>,
         children: [
             {
                 path: '/',
